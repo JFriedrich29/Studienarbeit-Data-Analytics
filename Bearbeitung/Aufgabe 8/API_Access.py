5588c7c32ca2f9567c7aa59b7218c0313faf8a0b
ckennerknecht <47369419+ckennerknecht@users.noreply.github.com>
2020-06-29 07:54:39 +0200

    API umgebaut, sodass sie in Aufgabe 2 verwendet werden kann

--- Bearbeitung/Aufgabe 8/API_Access.py
+++ Bearbeitung/Aufgabe 8/API_Access.py
@@ -46,59 +46,61 @@ def GetMeasurements_MeanPerHour_SingleComponent(station_id, component, date_from
     station_data_df = pd.DataFrame.from_dict(
         station_data_dict,
         orient="index",
-        columns=["component id", "scope id", "VALUE", "date end", "index"]
+        columns=["component id", "scope id", component, "date end", "index"]
     )
-
-    # Drop unnecessary columns
-    station_data_df.drop(
-        ['component id', 'scope id', 'date end', 'index'],
-        inplace=True,
-        axis=1
-    )
-
-    # Convert index to datetime
-    station_data_df = station_data_df.replace(
-        to_replace='24:00:00',
-        value="00:00:00",
-        regex=True
-    )
-    station_data_df.index = pd.to_datetime(station_data_df.index)
-    station_data_df.index.rename("DT", inplace=True)
 
     return station_data_df
 
 
 def GetMeasurements_MeanPerHour_MultiComponents(station_id, components, date_from, date_to):
 
-    # Create empty df with index
+    # Create empty df with index, that will be used to left join the single component data
     start_time = pd.to_datetime(date_from + ' 00:00:00',
                                 infer_datetime_format=True)
     end_time = pd.to_datetime(date_to + ' 23:00:00',
                               infer_datetime_format=True)
-    df_station_data = pd.DataFrame(pd.date_range(
+    df_multi_component = pd.DataFrame(pd.date_range(
         start_time, end_time, freq='h'), columns=["DT"])
-    df_station_data.set_index("DT", inplace=True)
+    df_multi_component.set_index("DT", inplace=True)
 
     for component in components:
         try:
-            df_datapoints = GetMeasurements_MeanPerHour_SingleComponent(
+            df_single_component = GetMeasurements_MeanPerHour_SingleComponent(
                 station_id=station_id,
                 component=component,
                 date_from=date_from,
                 date_to=date_to)
         except:
-            df_station_data.insert(
-                len(df_station_data.columns), component, np.NaN)
+            # If station provides no data, Insert empty column named after component
+            df_multi_component.insert(
+                len(df_multi_component.columns), component, np.NaN)
             continue
 
-        df_station_data = df_station_data.merge(
-            df_datapoints["VALUE"],
+        # Drop unnecessary columns
+        # df_single_component.drop(
+        #     ['component id', 'scope id', 'date end', 'index'],
+        #     inplace=True,
+        #     axis=1
+        # )
+
+        # Convert index to datetime
+        station_data_df = station_data_df.replace(
+            to_replace='24:00:00',
+            value="00:00:00",
+            regex=True
+        )
+        station_data_df.index = pd.to_datetime(station_data_df.index)
+        station_data_df.index.rename("DT", inplace=True)
+
+        # Left join single component data on datetime index
+        df_multi_component = df_multi_component.merge(
+            df_single_component[component],
             how='left',
             left_index=True,
             right_index=True,
         )
 
-        df_station_data.rename(
-            {"VALUE": component}, axis=1, inplace=True)
+        # df_multi_component.rename(
+        #     {"VALUE": component}, axis=1, inplace=True)
 
-    return df_station_data
+    return df_multi_component
